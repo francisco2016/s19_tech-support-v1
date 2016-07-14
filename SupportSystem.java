8d7a0359a883e7031a5e871661141054dbf385a5
francisco2016 <otrayama@gmail.com>
2016-07-14 11:33:59 +0200

    -0083 ahora suprime los posibles espacios en blanco que se pongan a la hora de escribir bye.

--- SupportSystem.java
+++ SupportSystem.java
@@ -33,12 +33,12 @@ public class SupportSystem
     public void start()
     {
         boolean finished = false;
-
+        
         printWelcome();
-
+        
         while(!finished) {
-            String input = reader.getInput();
-
+            String input = reader.getInput();//almaceno la repuesta del usuario en la VL.
+            input = input.trim();//aplico el mt trim() a la VL para eliminar los posibles espacios en blanco.(atrás y delante)
             if(input.startsWith("bye")) {
                 finished = true;
             }
